c20e7012f6e18b36dd92353d287cbb950832f1f3
psiconervio <psiconervio@gmail.com>
2023-11-28 20:18:04 -0300

    push

--- App-web-public/home.php
+++ App-web-public/home.php
@@ -200,7 +200,7 @@
           <h4 class="humidityColor"><i class="fas fa-tint"></i> HUMEDAD</h4>
           <p class="humidityColor"><span class="reading"><span id="ESP32_02_Humd"></span> &percnt;</span></p>
           <h4 class="anemometro_title"> <i class="fas fa-tint"></i>Anemometro</h4>
-          <p class="anemometro"><span class="reading"></span></p>
+          <p class="anemometro"><span id="Anemometro"></span></p>
           <!-- *********************************************************************** -->
           
           <p class="statusreadColor"><span>Estado Read Sensor DHT11 : </span><span id="ESP32_02_Status_Read_DHT11"></span></p>
